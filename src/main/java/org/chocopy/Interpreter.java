77aa84ccc1e3a715c044f0fe6475b8bebaac237e
Oleksandr Kravchuk <akravchuk89@gmail.com>
2023-03-02 19:24:43 +0200

    added statements

--- src/main/java/org/chocopy/Interpreter.java
+++ src/main/java/org/chocopy/Interpreter.java
@@ -1,6 +1,8 @@
 package org.chocopy;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
@@ -102,10 +104,11 @@ class Interpreter implements Expr.Visitor<Object> {
         throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
-    void interpret(Expr expression) {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             ChocoPy.runtimeError(error);
         }
@@ -123,5 +126,22 @@ class Interpreter implements Expr.Visitor<Object> {
         }
 
         return object.toString();
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
     }
 }
--- src/main/java/org/chocopy/utils/GenerateAst.java
+++ src/main/java/org/chocopy/utils/GenerateAst.java
@@ -19,6 +19,11 @@ public class GenerateAst {
                 "Literal  : Object value",
                 "Unary    : Token operator, Expr right"
         ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print      : Expr expression"
+        ));
     }
 
     private static void defineAst(
@@ -96,5 +101,6 @@ public class GenerateAst {
         }
 
         writer.println("    }");
+        writer.println();
     }
 }
