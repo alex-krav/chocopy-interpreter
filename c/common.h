08eae9366855b79ced40c998def2cffc129b9efe
Oleksandr Kravchuk <akravchuk89@gmail.com>
2023-04-24 14:38:50 +0300

    added bytecode

---
+++ c/common.h
@@ -0,0 +1,8 @@
+#ifndef chocopy_common_h
+#define chocopy_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
---
+++ c/debug.h
@@ -0,0 +1,9 @@
+#ifndef chocopy_debug_h
+#define chocopy_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
